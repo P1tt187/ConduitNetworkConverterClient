87f9f2f23f6addf77549d1f79615c200f6d7a8f1
fabian <f.markert87@googlemail.com>
2012-08-06 20:16:50 +0200

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Conduit
+import Data.Conduit.Network as Nw
+import Data.Conduit.List as Cl
+import Data.Conduit.Text as Ct
+import Data.Conduit.Binary as Cb
+
+main::IO()
+main = runTCPClient (ClientSettings 4000 "127.0.0.1") client
+
+client::Nw.Application IO
+client _ sink = runResourceT $ Cb.sourceFile "someFile.txt" $$ Cb.lines =$ Nw.sinkSocket sink
+        
