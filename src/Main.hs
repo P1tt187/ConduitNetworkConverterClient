e3513c22dc94f7709d3068ca07adcb6cbaf057fa
Fabian Markert <f.markert87@googlemail.com>
2012-08-28 00:42:22 +0200

    still bugs

--- src/Main.hs
+++ src/Main.hs
@@ -11,6 +11,8 @@ import Data.Conduit.Binary as Cb
 main::IO()
 main = runTCPClient (ClientSettings 4000 "127.0.0.1") client
 
-client::Nw.Application IO
-client _ sink = runResourceT $ Cb.sourceFile "someFile.txt" $$ Cb.lines =$ sink
+client::Pipe, IO t => Nw.Application t
+client _ sink = do
+  source <- Cb.sourceFile "someFile.txt"
+  sink $$ source
         
