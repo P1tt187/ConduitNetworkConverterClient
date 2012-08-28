2f5b36b26d82c10fded7707ac7ce03e1bddf8a36
fabian <f.markert87@googlemail.com>
2012-08-28 18:47:34 +0200

    try to fix some bugs

--- src/Main.hs
+++ src/Main.hs
@@ -7,12 +7,12 @@ import Data.Conduit.Network as Nw
 import Data.Conduit.List as Cl
 import Data.Conduit.Text as Ct
 import Data.Conduit.Binary as Cb
+import Data.Conduit.Blaze as Cbl
 
 main::IO()
 main = runTCPClient (ClientSettings 4000 "127.0.0.1") client
 
-client::Pipe, IO t => Nw.Application t
-client _ sink = do
-  source <- Cb.sourceFile "someFile.txt"
-  sink $$ source
+client::Nw.Application IO
+client _ sink = do  
+   transPipe $ Cb.sourceFile "someFile.txt" $$ sink
         
